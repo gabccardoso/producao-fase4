71410a2db1b4750ad9e2160bc4aa475827303493
gabrielasousa <gabrielasousa@touchtec.com.br>
2024-05-20 18:33:22 -0300

    Atualizando credenciais

--- src/main/java/com/fiap/producao/main/RabbitMQConfig.java
+++ src/main/java/com/fiap/producao/main/RabbitMQConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    private final String rabbitHost = "ec2-100-26-122-241.compute-1.amazonaws.com";
+    private final String rabbitHost = "ec2-100-25-119-145.compute-1.amazonaws.com";
     private final String rabbitUsername = "guest";
     private final String rabbitPassword = "guest";
 
